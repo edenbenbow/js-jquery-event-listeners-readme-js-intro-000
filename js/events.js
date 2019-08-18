f928f42fbb6a4133d48ecc9014e904d83c8b1384
E.B <edenbenbow@yahoo.com>
2019-08-18 02:24:00 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ $("img").addClass("tasty");
 }
 
 function pressIt() {
-  $(document).on('keydown', function() {
+  $(document).on('keydown', function(key) {
     if (key.which == 71) {
       alert('G was pressed');
    }
